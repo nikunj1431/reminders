6b2c8c0925bdecf9947d47ea9382cbf3e80401c8
nikunj1431 <73771369+nikunj1431@users.noreply.github.com>
2020-11-19 14:55:44 +0530

    Added the facility to add to startup
    Version 2.2.0

--- Reminder/custom_reminder.py
+++ Reminder/custom_reminder.py
@@ -5,26 +5,40 @@ hour = ""
 minute = ""
 message = ""
 title = ""
-timeout = 30
+day = ""
+month = ""
+other_day = False
 
-def set_reminder():
+def set_reminder():#This method inputs all the details needed for the reminder.
+
+    if input("Do you want the reminder for today or any other day?(t/o)") == "o":
+        global day,month,other_day
+        month = input("Which month do you want the reminder for(1 for January, 12 for december)")
+        day = input("What date do you want the reminder for?")
+        other_day = True
     print("At what time do you want to set the reminder for?")
-    global hour, minute,message,title, timeout
+    global hour, minute,message,title
     hour = input("Enter the hour in 24 hour format")
     minute = input("Enter the minute")
     title = input("About what do you want the notification to be?")
     message = input("What message do you want in your notification?")
-    timeout = int(input("(Optional) After how many seconds should the notification dissapear")
 
-def is_time():
+
+def is_time_custom():#checks if it is time yet to send the notification
+    if other_day == True:
+        if datetime.datetime.now().strftime("%d") == day and datetime.datetime.now().strftime("%m") == month:
+            pass#If it is the day to send the reminder, it will continue
+        else:
+            return False# If not, it will immediateky returen False
+    # The following checks if the it is the day to send the reminder
     time_now = datetime.datetime.now()
     if time_now.strftime("%H") == hour and time_now.strftime("%M") == minute:
         return True
     else:
         return False
 
-def send_notification():
+def send_notification():#sends the notification
     notification.notify(title = title,
                         message = message,
-                        timeout = timeout,
-                        app_icon = r"D:\Downloads\reminder.ico")
+                        timeout = 30,
+                        app_icon = "reminder.ico")
--- Reminder/reminder.py
+++ Reminder/reminder.py
@@ -2,10 +2,15 @@ from plyer import notification
 import datetime
 from time import sleep
 import custom_reminder
+import getpass
+import os
 
+def add_to_startup():
+    with open(r"C:\ProgramData\216c40503e\reminder.bat","w") as file:
+        file.write("Start \"\" %s "%__file__)
 
-
-def is_time():
+last_reminder_sent = int(datetime.datetime.now().strftime("%M"))
+def is_time():#checks if it is time yet for the notification to be sent
     current_time = int(datetime.datetime.now().strftime("%M"))
     if last_reminder_sent + interval == current_time:
         return True
@@ -14,19 +19,27 @@ def is_time():
     else:
         return False
 
+# The following is the main logic of the program.
 if input("Do you want a reminder at a fixed time?(y/n)") == "y":
     custom_reminder.set_reminder()
+
+
+if input("Do you want the program to start every time you start the computer?(y/n)") == "y" and os.path.exists("C:\ProgramData\216c40503e\reminder.bat") == False:
+    print("Please start the program again")
+    sleep(10)
+    add_to_startup()
+    exit(0)
+
 interval = int(input("After how many minutes do want a reminder?"))
-last_reminder_sent = int(datetime.datetime.now().strftime("%M"))
 while True:
     if datetime.datetime.now().strftime("%S") == "00":
-        if custom_reminder.is_time() == True:
+        if custom_reminder.is_time_custom() == True:
             custom_reminder.send_notification()
         if is_time() == True:
             notification.notify(title = "Take a break",
                                 message = "Take a break, rest your eyes, move around and drink water",
                                 timeout = 30,
-                                app_icon = "D:\\Downloads\\remindericon.ico")
+                                app_icon = "remindericon.ico")
             last_reminder_sent = int(datetime.datetime.now().strftime("%M"))
         else:
             sleep(60)
